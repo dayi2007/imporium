8f5ae02aee517dbc769a7aee63468a8733db8fa7
Andrew Lee <leeandrew@usa.com>
2021-11-15 11:57:44 -0500

    complete home.js screen and working on carousel.js component.

---
+++ client/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import './Carousel.css'
+import ProductCard from '../ProductCard/ProductCard'
+import { getProducts } from '../../services/products'
+
+const Carousel = () => {
+    
+    return (
+        <div className="carousel-div">
+
+        </div>
+    )
+}
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,1 +1,15 @@
-imporium
+import './Home.css'
+import { Layout, Carousel } from '../../components'
+
+const Home = (props) => {
+    
+return (
+    <Layout user={props.user}>
+        <div className='home'>
+            <Carousel />
+        </div>
+    </Layout>
+)
+}
+
+export default Home
