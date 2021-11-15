a6eb63878723add43c8d65c4e2b47f70475aabd5
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-11-15 10:46:01 -0500

    product detail code done - testing not complete

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from 'react';
+import Layout from '../../components/Layout/Layout.jsx';
+import { useParams } from 'react-router-dom';
+import { deleteProduct, getProduct } from '../../services/products.js';
+import { updateProduct } from '../../services/products.js';
+import './ProductDetail.css';
 
 
 const ProductDetail = () => {
+
+  const [product, setProduct] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const { id } = useParams;
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const product = await getProduct(id)
+      setProduct(product)
+      setLoaded(true)
+    }
+    fetchProduct()
+  }, [id])
+
+  if (!isLoaded) {
+    return <h1 className="fetch-product">Rendering NFT</h1>
+  }
+
   return (
-    <></>
+    <Layout>
+    <div className="product-detail-page">
+        <FullCard product={product}/>
+        <TokenData product={product} updateProduct={updateProduct} deleteProduct={deleteProduct}/>
+      </div>
+      </Layout>
   )
 }
 
