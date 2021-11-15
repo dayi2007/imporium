f22a493c03efe09aff139978e240eb922e6e74d7
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-11-15 10:55:49 -0500

    completed FullCard Component -- Testing still required

--- client/src/components/FullCard/FullCard.jsx
+++ client/src/components/FullCard/FullCard.jsx
@@ -1,8 +1,12 @@
 
 
-const FullCard = () => {
+const FullCard = ({product}) => {
   return (
-    <></>
+    <div className="full-card">
+      <img className="full-image" src={product.image} />
+      <h2 className="full-title">{product.title}</h2>
+      <h3 className="full-creator">{product.creator}</h3>
+    </div>
   )
 }
 
