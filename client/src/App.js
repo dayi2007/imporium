f5c0a505447652db554128d9dbf0239bb28ac955
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-11-15 10:54:44 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Routes, Redirect } from "react-router-dom";
+import "./App.css";
+import About from "./screens/About/About";
+import Home from "./screens/Home/Home";
+import Gallery from "./screens/Gallery/Gallery";
+import ProductCreate from "./screens/ProductCreate/ProductCreate";
+import ProductDetail from "./screens/ProductDetail/ProductDetail";
+import ProductEdit from "./screens/ProductEdit/ProductEdit";
+import SignIn from "./screens/SignIn/SignIn";
+import SignOut from "./screens/SignOut/SignOut";
+import SignUp from "./screens/SignUp/SignUp";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<Home user={user} />} />
+        <Route path="/sign-up" element={<SignUp setUser={setUser} />} />
+        <Route path="/sign-in" element={<SignIn setUser={setUser} />} />
+        <Route path="/sign-out" element={<SignOut setUser={setUser} />} />
+        <Route exact path="/gallery" element={<Gallery user={user} />} />
+        <Route
+          path="/create-product"
+          element={
+            user ? <ProductCreate user={user} /> : <Redirect to="sign-up" />
+          }
+        />
+        <Route
+          exact
+          path="/gallery/:id/edit"
+          element={
+            user ? <ProductEdit user={user} /> : <Redirect to="/gallery" />
+          }
+        />
+        <Route exact path="/gallery/:id" element={<ProductDetail user={user} />} />
+        <Route exact path="/about" element={<About user={user} />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
