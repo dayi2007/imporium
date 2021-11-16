1c958bf37e7d35c05ab434978e182e9ab0a03f8c
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-11-16 14:56:42 -0500

    Update simple styling

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './SignIn.css'
 import { signIn } from '../../services/users'
 import { useNavigate } from 'react-router-dom'
+import Layout from '../../components/Layout/Layout.jsx';
 
 const SignIn = (props) => {
   const history = useNavigate()
@@ -56,6 +57,8 @@ const SignIn = (props) => {
   const { username, password } = form
 
   return (
+    <div>
+      <Layout>
     <div className='form-container'>
       <h3>Sign In</h3>
       <form onSubmit={onSignIn}>
@@ -79,6 +82,8 @@ const SignIn = (props) => {
         />
         {renderError()}
       </form>
+      </div>
+      </Layout>
     </div>
   )
 }
